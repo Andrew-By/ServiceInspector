6d88b956cd6a70f54da7c0b7f3f425fa12981156
Андрей Бычков <andrew-by@hotmail.com>
2021-02-11 20:45:33 +0300

    Добавлено обнаружение циклов в задаче 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -11,21 +11,22 @@ namespace Task1
         static void Main(string[] args)
         {
             int n = 4; // Количество чиновников
-            var references = new HashSet<int>(); // Справки, которые можно получить
+            var orders = new HashSet<int>(); // Справки, которые можно получить
             var officers = new Dictionary<int, int[]>(); // Зависимости между чиновниками
+            bool hasLoop = false; // Флаг наличия циклической зависимости
             officers.Add(1, new[] { 2 });
             officers.Add(2, new[] { 3, 4 });
             int current = 1;
-            while (references.Count < n)
+            while (orders.Count < n)
             {
-                if (!references.Contains(current))
+                if (!orders.Contains(current))
                 {
                     bool hasAll = true;
                     if (officers.ContainsKey(current))
                     {
                         foreach (var depends in officers[current])
                         {
-                            if (!references.Contains(depends))
+                            if (!orders.Contains(depends))
                             {
                                 hasAll = false;
                                 break;
@@ -34,15 +35,26 @@ namespace Task1
                     }
 
                     if (hasAll)
-                        references.Add(current);
+                    {
+                        hasLoop = false;
+                        orders.Add(current);
+                    }
 
                 }
                 if (current < n)
                     current++;
                 else
+                {
+                    if (hasLoop)
+                    {
+                        Console.WriteLine("В зависимостях обнаружен цикл!");
+                        return;
+                    }
+                    hasLoop = true;
                     current = 1;
+                }
             }
-            Console.WriteLine(string.Join(" ", references));
+            Console.WriteLine(string.Join(" ", orders));
         }
     }
 }
